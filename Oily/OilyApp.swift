b50a9daeb7ed503c7a8e04342ad4eafcab769e0c
soheil pakgohar <soheilpakgohar@gmail.com>
2024-05-28 09:28:42 +0330

    Initial Commit

---
+++ Oily/OilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OilyApp.swift
+//  Oily
+//
+//  Created by soheil pakgohar on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct OilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
